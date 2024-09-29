c519a5eddc69597bbf41477e6fe9b47d646915ff
Divyanshu Shekhar <114979302+Divyanshu-Shekha@users.noreply.github.com>
2024-09-29 15:11:16 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [length, setLength] = useState(9);
+  const [numberallowed, setNumberallowed] = useState(false);
+  const [charallowed, setCharallowed] = useState(false);
+  const [passWord, setPassword] = useState("");
+
+  const passWordgenerator = useCallback(() => {
+    let pass = "";
+    let str = "abcdefghijklmnopqrstuvwxyz";
+    if (numberallowed) {
+      str += "0123456789";
+    }
+    if (charallowed) {
+      str += "@!#$%^&*";
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let randomval = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(randomval);
+    }
+    setPassword(pass);
+  }, [numberallowed, charallowed, setPassword, length]);
+
+  useEffect(() => {
+    passWordgenerator();
+  }, [passWordgenerator]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random PassWord Generator</h1>
+      <div className="main_container">
+        <div className="text_container">
+          {" "}
+          <input type="text" value={passWord}  className="password_container"/>
+          <input
+            type="button"
+            value="copy"
+            className="button_container"
+            onClick={() => {
+              navigator.clipboard.writeText(passWord);
+            }}
+          />
+        </div>
+        <div className="range_container">
+          <input
+            type="range"
+            defaultValue={length}
+            onChange={(e) => setLength(e.target.value)}
+          />
+          <h3>The length is : {length}</h3>
+        </div>
+        <div className="checkContainer">
+          <label for="number">Number = </label>
+          <input
+            type="checkbox"
+            defaultChecked={numberallowed}
+            name="number"
+            onChange={() => {
+              setNumberallowed((prev) => !prev);
+            }}
+          />
+          <br />
+          <label for="charecter">Charecter = </label>
+          <input
+            type="checkbox"
+            defaultChecked={charallowed}
+            name="charecter"
+            onChange={() => {
+              setCharallowed((prev) => !prev);
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
